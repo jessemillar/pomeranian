a730834146b5d87ca0be77c9cd6b3d4eb6b234cc
Jesse Millar <hellojessemillar@gmail.com>
2015-10-13 09:35:27 -0600

    Tweaking JavaScript

--- controller/scripts/main.js
+++ controller/scripts/main.js
@@ -1,5 +1,11 @@
-var left_stick = document.getElementById('left-stick');
-var left_nub = document.getElementById('left-nub');
+var left_stick = document.getElementById('left-stick'),
+    left_nub = document.getElementById('left-nub'),
+    stick_width = document.getElementById('left-stick').offsetWidth,
+    stick_height = document.getElementById('left-stick').offsetHeight,
+    nub_width = document.getElementById('left-nub').offsetWidth,
+    nub_height = document.getElementById('left-nub').offsetHeight;
+
+console.log(stick_width);
 
 left_stick.addEventListener('touchend', function(event) {
     left_nub.style.left = touch.pageX + 'px';
@@ -9,7 +15,7 @@ left_stick.addEventListener('touchend', function(event) {
 left_stick.addEventListener('touchmove', function(event) {
     if (event.targetTouches.length == 1) {
         var touch = event.targetTouches[0];
-        left_nub.style.left = touch.pageX + 'px';
-        left_nub.style.top = touch.pageY + 'px';
+        left_nub.style.left = touch.pageX - nub_width / 2 + 'px';
+        left_nub.style.top = touch.pageY - nub_height / 2 + 'px';
     }
 }, false);
