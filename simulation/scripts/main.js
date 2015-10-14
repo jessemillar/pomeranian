6c604e8d9d21f24ccf98d4463ca6784fb5300522
Jesse Millar <hellojessemillar@gmail.com>
2015-10-14 17:25:50 -0600

    Adding rudimentary controls

--- simulation/scripts/main.js
+++ simulation/scripts/main.js
@@ -5,7 +5,7 @@ var scale = 10,
     drone_height = 0.01 * scale,
     drone_width = 0.02 * scale,
     motor_diameter = 0.008 * scale,
-    starting_height = 2.5 * scale,
+    starting_height = 0 * scale,
     drone_body_weight = 0.2 * scale,
     drone_motor_weight = 0.1 * scale,
     drone_weight = (drone_body_weight + drone_motor_weight * 4) * scale,
@@ -15,18 +15,19 @@ var scale = 10,
     floor_thickness = 0.5 * scale,
     motor_power = true,
     motor_level = 2, // Modifier of gravity
-    motor_increment = 0.0025 * scale,
+    motor_increment = 0.0005 * scale,
+    initial_thrust = gravity_strength / 4 * motor_level,
     motor_thrust = [{ // angle gets automatically computed in api.js
-        force: 0,
+        force: initial_thrust,
         angle: 0
     }, {
-        force: 0,
+        force: initial_thrust,
         angle: 0
     }, {
-        force: 0,
+        force: initial_thrust,
         angle: 0
     }, {
-        force: 0,
+        force: initial_thrust,
         angle: 0
     }],
     debug = true,
@@ -35,8 +36,8 @@ var scale = 10,
 document.addEventListener(
     'keydown',
     function(event) {
-        if (event.keyCode == 13) { // Use this mostly when you're hovering in a stable state
-            console.log("Adding death box");
+        if (event.keyCode == 13) { // "Enter" key
+            console.log("Adding death box"); // Use this mostly when you're hovering in a stable state
 
             var death_box = new Physijs.BoxMesh(
                 new THREE.BoxGeometry(drone_height, drone_height, drone_height),
@@ -52,15 +53,29 @@ document.addEventListener(
             death_box.setCcdSweptSphereRadius(drone_height / 2);
 
             scene.add(death_box);
-        } else if (event.keyCode == 32) {
+        } else if (event.keyCode == 32) { // Spacebar
             motor_power = !motor_power;
             console.log("Toggling motors");
-        } else if (event.keyCode == 38) {
-            motor_level += motor_increment;
+        } else if (event.keyCode == 38) { // Up arrow
+            motor_thrust[0].force += motor_increment;
+            motor_thrust[1].force += motor_increment;
+            motor_thrust[2].force += motor_increment;
+            motor_thrust[3].force += motor_increment;
             console.log("Increasing motor power");
-        } else if (event.keyCode == 40) {
-            motor_level -= motor_increment;
+        } else if (event.keyCode == 40) { // Down arrow
+            motor_thrust[0].force -= motor_increment;
+            motor_thrust[1].force -= motor_increment;
+            motor_thrust[2].force -= motor_increment;
+            motor_thrust[3].force -= motor_increment;
             console.log("Decreasing motor power");
+        } else if (event.keyCode == 65) { // "A" key
+            portImpulse();
+        } else if (event.keyCode == 68) { // "D" key
+            starboardImpulse();
+        } else if (event.keyCode == 83) { // "S" key
+            backwardImpulse();
+        } else if (event.keyCode == 87) { // "W" key
+            forwardImpulse();
         }
     }
 );
@@ -69,18 +84,29 @@ var main = function() {
     console.log(getPosition(), getTilt());
 
     if (motor_power) {
-        hover();
+        impulseMotors();
     }
 
     safetySwitch();
     helperArrows();
 };
 
-var hover = function() {
-    motor_thrust[0].force = gravity_strength / 4 * motor_level;
-    motor_thrust[1].force = gravity_strength / 4 * motor_level;
-    motor_thrust[2].force = gravity_strength / 4 * motor_level;
-    motor_thrust[3].force = gravity_strength / 4 * motor_level;
+var forwardImpulse = function() {
+    motor_thrust[2].force += motor_increment;
+    motor_thrust[3].force += motor_increment;
+};
 
-    impulseMotors();
+var backwardImpulse = function() {
+    motor_thrust[0].force += motor_increment;
+    motor_thrust[1].force += motor_increment;
+};
+
+var portImpulse = function() {
+    motor_thrust[0].force += motor_increment;
+    motor_thrust[3].force += motor_increment;
+};
+
+var starboardImpulse = function() {
+    motor_thrust[1].force += motor_increment;
+    motor_thrust[2].force += motor_increment;
 };
